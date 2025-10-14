dd0430372fb2009a87e7d7d3b038d82c18dd3d36
Sumit <sumsri2024@gmail.com>
2025-10-15 01:03:37 +0530

    Add ThemeProvider and SmoothCursor components; implement AnimateOnScroll for home sections; enhance Hero with typing animation; update index.css for cursor styles.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,49 @@
+// src/pages/Home.jsx
+
 import React from "react";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Hero from '../components/Hero';
 import Features from "../HomePage_Components/Features";
-import ChooseUs from "../HomePage_Components/ChooseUs";
 import HowItWorks from "../HomePage_Components/HowItWorks";
-import Pricing from "../HomePage_Components/Pricing";
-import {Link} from 'react-router-dom'
-import Hero from '../components/Hero'
 import DashboardPreview from "../HomePage_Components/DashboardPreview";
+import ChooseUs from "../HomePage_Components/ChooseUs";
+import Pricing from "../HomePage_Components/Pricing";
 import FinalCTA from "../HomePage_Components/FinalCTA";
+
+// Import your new wrapper component
+import AnimateOnScroll from "../components/ui/AnimateOnScroll";
+
 const Home = () => {
-
-
   return (
     <>
-    <Hero/>
-    <Features/>
-    <HowItWorks/>
-    <DashboardPreview/>
-    <ChooseUs/>
-    <Pricing/>
-    <FinalCTA/>
-    </>
+      {/* The Hero is usually visible on load, so it may not need scroll animation */}
+      <Hero />
 
+      {/* Wrap each subsequent section with the AnimateOnScroll component */}
+      <AnimateOnScroll>
+        <Features />
+      </AnimateOnScroll>
+
+      <AnimateOnScroll>
+        <HowItWorks />
+      </AnimateOnScroll>
+
+      <AnimateOnScroll>
+        <DashboardPreview />
+      </AnimateOnScroll>
+
+      <AnimateOnScroll>
+        <ChooseUs />
+      </AnimateOnScroll>
+
+      <AnimateOnScroll>
+        <Pricing />
+      </AnimateOnScroll>
+      
+      <AnimateOnScroll>
+        <FinalCTA />
+      </AnimateOnScroll>
+    </>
   );
 };
+
 export default Home;
